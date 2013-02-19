4a6509095ef183e43734d43e7f57b2e0f1648863
asholok <asholok@gmail.com>
2013-02-19 15:32:27 +0200

    Formating

--- app.php
+++ app.php
@@ -29,57 +29,37 @@ class NumToWords
 		$lessThanHun 					= 	 $rev % 100;
 		$nums 							= 	 $lessThanHun % 10;
 		$teens							=	 floor($lessThanHun/10);
-			if($rev!=0){// if whole block $arr[$y] == 0
-				if($rev>=100)	{
-
+			if ($rev!=0)	{// if whole block $arr[$y] == 0 protection
+				if ($rev>=100)	{
 					$numOfHundred 		= 	 floor($rev/100);
 					$key    			=	 $key.$hundred[$numOfHundred];
-
 				}
-
-				if($lessThanHun >=20){
-
+				if ($lessThanHun >=20)	{
 						$numOfTens		= 	 floor($lessThanHun/10);
 						$key			=	 $key.$tens[$numOfTens];
-
-					if($y>=2){
-						if($nums==1 || $nums ==2){//gender sensivity
-
+					if ($y>=2)	{
+						if ($nums==1 || $nums ==2)	{//gender sensivity
 							$key 		=	 $key.$upper[$nums];
-
-						}else{
-
+						} else {
 							$key		=	 $key.$toTwenty[$nums];
-
 						}
-					}else{
-
-							$key		=	 $key.$toTwenty[$nums];
+					} else {
+						$key			=	 $key.$toTwenty[$nums];
 					}
-
-
-				}elseif ($lessThanHun < 20 && $lessThanHun!=0) {
-					if($y>=2){
-						if($lessThanHun==1 || $lessThanHun ==2){//gender sensivity
-
+				} elseif ($lessThanHun < 20 && $lessThanHun!=0) {
+					if ($y>=2)	{
+						if ($lessThanHun==1 || $lessThanHun ==2)	{//gender sensivity
 							$key 		=	 $key.$upper[$lessThanHun];
-
-						}else{
-
+						} else {
 							$key		= 	 $key.$toTwenty[$lessThanHun];
-
 						}
-					}else{
-
-							$key		= 	 $key.$toTwenty[$lessThanHun];
-
+					} else {
+						$key			= 	 $key.$toTwenty[$lessThanHun];
 						}		
 				}
-			
 				if($y>=2){// name num > 1 mln
 					if($teens!=1){
 						if($nums==1){
-
 							$key		= 	 $key.$mlnName[$y].$endings[0];
 						}elseif($nums>1 && $nums<5){
 							$key		= 	 $key.$mlnName[$y].$endings[1];
@@ -89,71 +69,58 @@ class NumToWords
 					}else{
 							$key 		= 	 $key.$mlnName[$y].$endings[2];
 					}
-
 				}if ($y==1) {// thuosend plural names
 					if($teens!=1){
 						if($nums==1){
-
 						$key 			= 	 $key.$thousName.$endings[9];
-
 						}elseif($nums>1 && $nums<5){
-
 						$key 			= 	 $key.$thousName.$endings[10];
-
 						}else{
-
 						$key 			= 	 $key.$thousName.$endings[0];
-
 						}
 					}else{
 						$key 			= 	 $key.$thousName.$endings[0];
 					}
 				}
-
 			} // Here ends condition if block $arr[$y] == 0
 		// adding currensy plural names
 			if ($y==0) {
-				if($teens!=1){
-					if($nums==1){
+				if ($teens!=1) {
+					if ($nums==1) {
 						$key 			= 	 $key.$uahName.$endings[3];
-					}elseif($nums>1 && $nums<5){
+					} elseif ($nums>1 && $nums<5){
 						$key 			= 	 $key.$uahName.$endings[4];
-					}else{
+					} else {
 						$key 			= 	 $key.$uahName.$endings[5];
 					}
-				}else{
+				} else {
 						$key 			= 	 $key.$uahName.$endings[5];
 				}
-
-			}
-
-				
-
+			}			
 		}
 
 	// coins quantity 
-		if(strlen($firsrS[1])==1){//one digit protection (0.5 == "fifty" not "five")
+		if (strlen($firsrS[1])==1) {//one digit protection (0.5 == "fifty" not "five")
 						$key			= 	 $key.", ".$tens[$firsrS[1]];
 						$key 			= 	 $key.$coinName.$endings[8];// coins plural names
-		}else{
-			if($firsrS[1] >=20){
+		} else {
+			if ($firsrS[1] >=20) {
 
 						$numOfTens		= 	 floor($firsrS[1]/10);
 						$key			=	 $key.", ".$tens[$numOfTens];
 						$nums 			= 	 $firsrS[1] % 10;
 						$key			=	 $key.$toTwenty[$nums];
 				
-			}else{
+			} else {
 						$withOutZero	=	 ($firsrS[1]/10)*10;
 						$key			= 	 $key.", ".$toTwenty[$withOutZero];
-
 			}
 	
-			if($firsrS[1]==1){// coins plural names
+			if ($firsrS[1]==1) {// coins plural names
 						$key 			= 	 $key.$coinName.$endings[6];
-				}elseif($firsrS[1]>1 && $firsrS[1]<5){
+				} elseif ($firsrS[1]>1 && $firsrS[1]<5) {
 						$key 			= 	 $key.$coinName.$endings[7];
-				}else{
+				} else {
 						$key 			= 	 $key.$coinName.$endings[8];
 			}
 
