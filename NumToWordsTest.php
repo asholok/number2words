cfccfa364a3546a10277832c3b8cc1926b7d6115
asholok <asholok@gmail.com>
2013-02-20 22:30:26 +0200

    add PHPUnit test

---
+++ NumToWordsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once 'NumToWords.php';
+
+class NumToWordsTest extends PHPUnit_Framework_TestCase {
+    public function checkTest(){
+
+    	$new = new NumToWords();
+    	
+    }
+}
