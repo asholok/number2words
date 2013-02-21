fb8efb473178967593f50301aa17e5294fabacba
asholok <asholok@gmail.com>
2013-02-21 10:39:59 +0200

    add test 4 check

--- NumToWordsTest.php
+++ NumToWordsTest.php
@@ -7,6 +7,13 @@ class NumToWordsTest extends PHPUnit_Framework_TestCase {
     public function checkTest(){
 
     	$new = new NumToWords();
+    	this->assertTrue($new->convert('111354543.77'));
+    	this->assertTrue($new->convert('1110000543.77'));
+    	this->assertTrue($new->convert('111354541'));
+    	this->assertTrue($new->convert('111354543.5'));
+    	this->assertTrue($new->convert('111354543.06'));
+    	this->assertEquals('двa мільярди один мільйон одна тисячa гривень , одинадцять копійок ',$new->convert('2001001000.11'));
     	
+
     }
 }
